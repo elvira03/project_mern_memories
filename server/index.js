37d6c416a43feeb18d5d17a2592f3ee4d02e80e8
elvira03 <ekasymbekova2003@gmail.com>
2023-09-30 08:09:09 +0600

    update db

--- server/index.js
+++ server/index.js
@@ -8,13 +8,14 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = "mongodb+srv://elvira2003:elvira2003@cluster0.xvdesnz.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
